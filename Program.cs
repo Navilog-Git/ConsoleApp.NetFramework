25daba1a6b23a1895b95801e76a87b26914c311e
sithumi <sithumi@chainx.se>
2020-12-28 09:33:49 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForJenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a .net framework console application");
+        }
+    }
+}
